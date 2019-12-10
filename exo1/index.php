1c25cfeab3f72bfa37368bb7ff0df1c0c977372e
francois80 <glavieux.francois@orange.fr>
2019-12-10 09:30:40 +0100

    Ajout exercice 1

---
+++ exo1/index.php
@@ -0,0 +1,12 @@
+<?php
+$exercice ='Exercice 1';
+include '../header.php';
+function returnTrue()
+{
+  return true;
+}
+?>
+<h1>Faire une fonction qui retourne true.</h1>
+<p><?= returnTrue(); ?></p>
+</body>
+</html>
